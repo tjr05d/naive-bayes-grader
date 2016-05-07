843897e1b44969ef7a39c1f49d18562d2c76275a
tjr05d <tjr05d@my.fsu.edu>
2016-05-07 15:40:20 -0400

    made endpoints for units, questiions, and categories

--- app.py
+++ app.py
@@ -18,8 +18,9 @@ class JsondModel(object):
         return {key: getattr(self, key) for key in self.dumb_ass_attrs}
 
 
-class Unit(db.Model):
+class Unit(db.Model, JsondModel):
     __tablename__ = 'units'
+    external_attrs = ['title', 'description']
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String())
@@ -35,20 +36,42 @@ class Unit(db.Model):
         return '<id {}>'.format(self.id)
 
 
-class Question(db.Model):
+class Question(db.Model, JsondModel):
     __tablename__ = 'questions'
+    external_attrs = ['title', 'prompt', 'unit_id']
 
     id = db.Column(db.Integer, primary_key=True)
-    prompt = db.Column(db.String())
+    title = db.Column(db.String())
+    prompt = db.Column(db.Text)
     unit_id = db.Column(db.Integer, db.ForeignKey('units.id'))
     responses = db.relationship('Response', backref='question', lazy= 'dynamic')
+    categories = db.relationship('Category', backref='question', lazy= 'dynamic')
 
-    def __init__(self, prompt, unit_id):
+    def __init__(self, title, prompt, unit_id):
+        self.title = title
         self.prompt = prompt
         self.unit_id = unit_id
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
+
+class Category(db.Model, JsondModel):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String())
+    feedback = db.Column(db.Text)
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'))
+    responses = db.relationship('Response', backref='category', lazy='dynamic')
+
+    def __init__(self, title, feedback, questions_id):
+        self.title = title
+        self.feedback = feedback
+        self.questions_id = questions_id
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
 
 
 class Response(db.Model, JsondModel):
@@ -57,17 +80,15 @@ class Response(db.Model, JsondModel):
     dumb_ass_attrs= ['answer','cat', 'feedback', 'id']
 
     id = db.Column(db.Integer, primary_key=True)
-    answer = db.Column(db.String())
-    cat = db.Column(db.String())
-    feedback = db.Column(db.String())
+    answer = db.Column(db.Text)
     active = db.Column(db.Boolean)
+    categories_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
     questions_id = db.Column(db.Integer, db.ForeignKey('questions.id'))
 
-    def __init__(self, answer, cat, feedback, questions_id):
+    def __init__(self, answer, active, categories_id, questions_id):
         self.answer = answer
-        self.cat = cat
-        self.feedback = feedback
         self.active = False
+        self.categories_id = categories_id
         self.questions_id = questions_id
 
     def __repr__(self):
@@ -80,7 +101,7 @@ class Response(db.Model, JsondModel):
     @property
     def classify_response(self):
         #test data point to pass into the classify method
-        test_response= ("Classes are cool", "Correct")
+        test_response= ("Something", "Correct")
 
         #pulls the training data from previous responses for this question
         # train = db.session.query(Response).filter(Response.questions_id==1)
@@ -100,7 +121,7 @@ class Response(db.Model, JsondModel):
         prob_cat = question.prob_classify(test_response)
         #loop to return the prob that the response falls in each of the categories
         cat_probabilities = []
-        #loop to get teh probability of all the categores that exist for that classifier
+        #loop to get the probability of all the categories that exist for that classifier
         for cat in question.labels():
             cat_probabilities.append((cat, prob_cat.prob(cat)))
         #return a hash with the category picked as well as the probabilities fo all the categories of the classifier
@@ -110,10 +131,10 @@ class Response(db.Model, JsondModel):
     def score(self):
         return {'methods':95, 'syntax':20}
 
-
+#responses routes
 @app.route('/grader/api/v1.0/responses', methods=['GET'])
 def get_responses():
-    return jsonify({'responses' : prepare_index_json()})
+    return jsonify({'responses' : prepare_index_json(Response)})
 
 
 @app.route('/grader/api/v1.0/responses/<int:response_id>', methods=['GET'])
@@ -129,6 +150,7 @@ def create_response():
         abort(400)
     response = Response(
         answer= request.json['answer'],
+        active = False,
         cat= request.json['cat'],
         feedback= request.json['feedback']
         )
@@ -136,14 +158,89 @@ def create_response():
     db.session.commit()
     return jsonify({'response': response.to_dict}), 201
 
+#categories routes
+@app.route('/grader/api/v1.0/categories', methods=['GET'])
+def get_categories():
+    return jsonify({'categories' : prepare_index_json(Category)})
+
+@app.route('/grader/api/v1.0/categories/<int:category_id>', methods=['GET'])
+def get_category(category_id):
+    category = Category.query.get(category_id)
+    if not category:
+        abort(404)
+    return jsonify({'category': category.to_dict})
+
+@app.route('/grader/api/v1.0/categories', methods=['POST'])
+def create_category():
+    if not request.json or not 'answer' in request.json:
+        abort(400)
+    category = Category(
+        title= request.json['title'],
+        feedback= request.json['feedback'],
+        question_id= request.json['question_id']
+        )
+    db.session.add(category)
+    db.session.commit()
+    return jsonify({'category': category.to_dict}), 201
+
+#Questions routes
+@app.route('/grader/api/v1.0/questions', methods=['GET'])
+def get_questions():
+    return jsonify({'questions' : prepare_index_json(Question)})
+
+@app.route('/grader/api/v1.0/questions/<int:question_id>', methods=['GET'])
+def get_question(question_id):
+    question = Question.query.get(question_id)
+    if not question:
+        abort(404)
+    return jsonify({'question': question.to_dict})
+
+@app.route('/grader/api/v1.0/questions', methods=['POST'])
+def create_question():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    question = Question(
+        title= request.json['title'],
+        prompt= request.json['prompt'],
+        unit_id= int(request.json['unit_id'])
+        )
+    db.session.add(question)
+    db.session.commit()
+    return jsonify({'question': question.to_dict}), 201
+
+#Unit routes
+@app.route('/grader/api/v1.0/units', methods=['GET'])
+def get_units():
+    return jsonify({'units' : prepare_index_json(Unit)})
+
+@app.route('/grader/api/v1.0/units/<int:unit_id>', methods=['GET'])
+def get_unit(unit_id):
+    unit = Unit.query.get(unit_id)
+    if not unit:
+        abort(404)
+    return jsonify({'unit': unit.to_dict})
+
+@app.route('/grader/api/v1.0/units', methods=['POST'])
+def create_unit():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    unit= Unit(
+        title= request.json['title'],
+        description= request.json['description'],
+        )
+    db.session.add(unit)
+    db.session.commit()
+    return jsonify({'unit': unit.to_dict}), 201
+
+#error handler route
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-def prepare_index_json():
-    responses = db.session.query(Response).all()
-    response_arr = [response.to_dict for response in responses]
-    return response_arr
+def prepare_index_json(request_type):
+    index_items = db.session.query(request_type).all()
+    index_items_arr = [item.to_dict for item in index_items]
+    return index_items_arr
 
 if __name__ == '__main__':
     app.run(debug=True)
